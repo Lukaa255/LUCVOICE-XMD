ef547ea2555326fb0e011eb2618f208e776e3629
Lukaa255 <lukabrand254@gmail.com>
2026-03-29 12:21:59 -0700

    Update forward.js

--- lucvoice/forward.js
+++ lucvoice/forward.js
@@ -23,7 +23,7 @@ zokou({ nomCom: "forward", categorie: 'Group', reaction: "📤" }, async (dest, 
       // Send the message directly to the member's DM
       await zk.sendMessage(memberNumber, { text: mess });
     }
-    repondre('*NEXUS-AI Forwarded Your Message To All group members in their DMs.*');
+    repondre('*LUCVOICE-XMD Forwarded Your Message To All group members in their DMs.*');
   } else {
     repondre('*Only My Owner Can Use This Command.*');
   }
