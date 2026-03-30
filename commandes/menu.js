a8d8335bcfd77042fcfcf1c12809cc2d2edf847b
Lukaa255 <lukabrand254@gmail.com>
2026-03-30 20:58:49 +0300

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -5,78 +5,74 @@ const { format } = require(__dirname + "/../framework/mesfonctions");
 const os = require("os");
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
-const more = String.fromCharCode(8206)
-const readmore = more.repeat(4001)
-zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-    let { cm } = require(__dirname + "/../framework//zokou");
-    var coms = {};
-    var mode = "public";
-    if ((s.MODE).toLocaleLowerCase() != "yes") {
-        mode = "private";
 
-    }
-    cm.map(async (com, index) => {
-        if (!coms[com.categorie])
-            coms[com.categorie] = [];
+const more = String.fromCharCode(8206);
+const readmore = more.repeat(4001);
+
+zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
+    let { cm } = require(__dirname + "/../framework/zokou");
+    let coms = {};
+    let mode = s.MODE.toLowerCase() === "yes" ? "public" : "private";
+
+    // Organiser commands par catégorie
+    cm.forEach(com => {
+        if (!coms[com.categorie]) coms[com.categorie] = [];
         coms[com.categorie].push(com.nomCom);
     });
+
     moment.tz.setDefault('Etc/GMT');
-// Créer une date et une heure en GMT
-const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD/MM/YYYY');
-  let infoMsg =  `
-╭━━〔 *${s.BOT}* 〕━━┈⊷
-┃๏╭───────────
-┃๏│▸ 𝗢𝘄𝗻𝗲𝗿 : ${s.OWNER_NAME}
-┃๏│▸ 𝗣𝗿𝗲𝗳𝗶𝘅 : [ ${s.PREFIXE} ] 
-┃๏│▸ 𝗠𝗼𝗱𝗲 : *${mode}*
-┃๏│▸ 𝗗𝗮𝘁𝗲  : *${date}* 
-┃๏│▸ 𝗥𝗮𝗺 : 𝟴/𝟭𝟯𝟮 𝗚𝗕
-┃๏│▸ 𝗣𝗹𝗮𝘁𝗳𝗼𝗿𝗺 : 𝗖𝗵𝗿𝗼𝗺𝗲 𝗟𝗶𝗻𝘂𝘅
-┃๏│▸ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿 : 𝗟𝘂𝗰𝘃𝗼𝗶𝗰𝗲
-┃๏└───────────···▸
-╰──────────────┈⊷\n${readmore}`;
-    let menuMsg = `-LUCVOICE-XMD is connected  
-`;
+    const temps = moment().format('HH:mm:ss');
+    const date = moment().format('DD/MM/YYYY');
+
+    // Header info
+    let infoMsg = `
+╭━━━〔 *${s.BOT}* 〕━━━┈⊷
+┃ 👑 Owner   : ${s.OWNER_NAME}
+┃ ⚡ Prefix  : [ ${s.PREFIXE} ] 
+┃ 🔐 Mode    : *${mode}*
+┃ 📅 Date    : *${date}*
+┃ ⏱️ Time    : *${temps}*
+┃ 💾 RAM     : 8/132 GB
+┃ 🖥️ Platform: Chrome Linux
+┃ 💡 Creator : Lucvoice
+╰────────────────┈⊷\n${readmore}`;
+
+    // Menu content
+    let menuMsg = `💫 *LUCVOICE-XMD COMMANDS* 💫\n`;
+
     for (const cat in coms) {
-        menuMsg += `
-╭──「 *${cat}* 」──┈⊷ 
-┃╭──────────
-┌┤ `;
-        for (const cmd of coms[cat]) {
-            menuMsg += `          
-┃│■  ${cmd}`    
-        } 
-        menuMsg +=`
-┌┤
-│╰────────┈⊷  
-╰────────────┈⊷`
+        menuMsg += `\n╭──「 *${cat.toUpperCase()}* 」──┈⊷\n`;
+        coms[cat].forEach(cmd => {
+            menuMsg += `┃ ✦ ${prefixe}${cmd}\n`;
+        });
+        menuMsg += `╰──────────────┈⊷\n`;
     }
-    menuMsg += `
-> LUCVOICE-XMD\n
-`;
-   var lien = mybotpic();
-   if (lien.match(/\.(mp4|gif)$/i)) {
+
+    menuMsg += `\n✨ Powered by LUKA iT\n`;
+
+    // Send with image/video if exists
+    let lien = mybotpic();
+
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
+        if (lien.match(/\.(mp4|gif)$/i)) {
+            await zk.sendMessage(dest, {
+                video: { url: lien },
+                caption: infoMsg + menuMsg,
+                footer: "Je suis *Beltahmd*, développeur Beltah Tech",
+                gifPlayback: true
+            }, { quoted: ms });
+        } else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+            await zk.sendMessage(dest, {
+                image: { url: lien },
+                caption: infoMsg + menuMsg,
+                footer: "Je suis *Beltahmd*, développeur Beltah Tech"
+            }, { quoted: ms });
+        } else {
+            await repondre(infoMsg + menuMsg);
+        }
+    } catch (e) {
+        console.log("🥵 Menu erreur: " + e);
+        repondre("🥵 Menu erreur: " + e);
     }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-// Vérification pour .jpeg ou .png
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-    try {
-       zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-else {
-    repondre(infoMsg + menuMsg);
-}
 });
